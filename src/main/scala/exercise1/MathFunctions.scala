d8fd10727a3f9fbb916418fce8ef38f83b92461c
Matthew Snaith <fault_line@hotmail.co.uk>
2014-06-30 10:03:36 +0100

    first commit

---
+++ src/main/scala/exercise1/MathFunctions.scala
@@ -0,0 +1,7 @@
+package exercise1
+
+class MathFunctions {
+
+  def square(number: List[Int]): List[Int] = ???
+  //Implementation is missing for this method. Once you have written your first test please write your implementation code
+}
---
+++ src/test/scala/exercise1/MathFunctionsSpec.scala
@@ -0,0 +1,37 @@
+package exercise1
+
+import spec.UnitSpec
+
+class MathFunctionsSpec extends UnitSpec {
+  "square" should {
+    "return 4 when 2 is input" in {
+      pending
+    }
+
+    "return a list of 4,9,16 when 2,3,4 are input" in {
+      pending
+    }
+
+    "return a list of 64,100,225,625,900 when 8,10,15,25,30 are input" in {
+      pending
+    }
+
+    "return a list of 25,36 when -5,-6 are input" in {
+      pending
+    }
+  }
+
+  "add0Point5" should {
+    "return 2.5 when 2 is input" in {
+      pending
+    }
+
+    "return 3.5, 6.5, 8.5 when 3, 6, 8 are input" in {
+      pending
+    }
+
+    "return -1.5, -9.5 when 2, 10 are input" in {
+      pending
+    }
+  }
+}
